0d43d449396d46e960f07f7ad413f91e8ce74efd
rodgeraraujo <rogerio.araujo@mail.com>
2019-03-03 19:32:52 -0300

    Fix connection to database, adding new host to phpmyadmin (MySQL)

--- Lirb/app/src/main/java/nf/co/rogerioaraujo/lirb/database/MySQL.java
+++ Lirb/app/src/main/java/nf/co/rogerioaraujo/lirb/database/MySQL.java
@@ -2,13 +2,23 @@ package nf.co.rogerioaraujo.lirb.database;
 
 public class MySQL {
 
+
+    /* host biz.nf (don't work)
     private final String HOST = "185.176.43.97"; // fdb26.biz.nf
     private final String DATA_BASE = "2975601_lirb";
     private final String PORT = "3306";
 
     private final String USER = "2975601_lirb";
     private final String PASSWORD = "lirb_321";
+    */
 
+    // host freesqldatabase
+    private final String HOST = "sql10.freesqldatabase.com";
+    private final String DATA_BASE = "sql10281687";
+    private final String PORT = "3306";
+
+    private final String USER = "sql10281687";
+    private final String PASSWORD = "fnbIiXbANd";
     private final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATA_BASE;
 
     public MySQL() {
